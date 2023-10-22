2910ffdaffc18204cd9e297182bb872692337428
Patricia <patricia.femenias@gmail.com>
2023-10-22 23:58:48 +0200

    Render PokemonsList component

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,6 @@
 import App from "./Components/App/App.js";
 import Button from "./Components/Button/Button.js";
+import PokemonsList from "./Components/PokemonsList/PokemonsList.test.js";
 import type { Pokemon } from "./types/types.js";
 import type { PokemonResponse } from "./types/types.js";
 
@@ -41,3 +42,6 @@ const buttonNext = new Button(
   },
 );
 buttonNext.render();
+
+const pokemonList = new PokemonsList(body, pokemons);
+pokemonList.render();
